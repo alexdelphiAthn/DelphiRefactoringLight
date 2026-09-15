268e04358bf33f80582fa0ff2b47326e121ad8bb
Sebastian Jänicke <sebastian.jaenicke@addipos.com>
2026-09-15 07:58:42 +0200

    fixed libsuffix and output dir

--- build.bat
+++ build.bat
@@ -2,7 +2,6 @@
 call "C:\Program Files (x86)\Embarcadero\Studio\37.0\bin\rsvars.bat"
 cd /d "%~dp0"
 if not exist DCU mkdir DCU
-if not exist Output2 mkdir Output2
-msbuild Packages\DelphiRefactoringLight.dproj /t:Build /p:Config=Debug /p:Platform=Win32 /p:DCC_BplOutput=.\Output2 /v:minimal
+msbuild Packages\DelphiRefactoringLight.dproj /t:Build /p:Config=Debug /p:Platform=Win32 /v:minimal
 echo.
 echo Exit Code: %ERRORLEVEL%
